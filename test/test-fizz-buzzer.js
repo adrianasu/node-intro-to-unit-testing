29fbaae0b2ff28cb5047c6831a0a1eb6e9aa0036
Adriana Suarez <agsmq@yahoo.com>
2018-08-27 13:39:11 -0500

    Added not multiples test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -6,7 +6,7 @@ const expect = require('chai').expect;
 //unit tests for fizzBuzzer
 describe('fizzBuzzer', function() {
 
-    //test normal case
+    //test normal case multiples of 3, 5 and 15
     it('should return fizz, buzz, fizz-buzz', function() {
         const normalCases = [
             {a: 6, expected: "fizz"},
@@ -16,6 +16,15 @@ describe('fizzBuzzer', function() {
         normalCases.forEach((input) => {
             const answer = fizzBuzzer(input.a);
             expect(answer).to.equal(input.expected);
+        });
+    });
+
+    //test normal case not multiples of 3 or 5
+    it('should return number if not multiple of 3 or 5', function () {
+        const notMultiple = [1, 2, 4];
+        notMultiple.forEach((input)=> {
+            const answer = fizzBuzzer(input);
+            expect(answer).to.equal(input);
         });
     });
 
